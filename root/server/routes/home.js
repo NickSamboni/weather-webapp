ea062590ea38a9c13a6d5d8fba573d5ea9199f02
Nick Ramos Samboni <ramossamboni@gmail.com>
2021-11-22 23:22:03 +0100

    implementing htmls for sign and home

--- root/server/routes/home.js
+++ root/server/routes/home.js
@@ -6,12 +6,20 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', function(req, res) { 
+  res.render('home'); // view name to be displayed
+});
+
+router.get('/index.ejs', function(req, res) { 
   res.render('index'); // view name to be displayed
 });
 
-router.get('/home', function(req, res) { 
-  res.render('home'); // view name to be displayed
+router.get('/signup.ejs', function(req, res) { 
+  res.render('signup'); // view name to be displayed
 });
+
+
+
+
 
 
 module.exports = router; // export the module to be able to access it outside this path
