06b235b34d1d77f40c502c16c1e3fb9e2cd4c6b1
Nick Ramos Samboni <ramossamboni@gmail.com>
2022-01-20 13:03:20 +0100

    server side for fetch the data into mongo, delete a bunch, and modifyanother bunch

--- react-app/server/index.js
+++ react-app/server/index.js
@@ -24,51 +24,134 @@ app.use(express.json());
 
 app.use('/user', user);
 
+
+const states = [ "AL", "AK", "AS"];
+
+const cors_domain = 'https://the-ultimate-api-challenge.herokuapp.com';
+const baseIPA = 'https://api.weather.gov/stations?state=';
+//const request_url = `${cors_domain}/${baseIPA}`;
+const latest_obs1 = 'https://api.weather.gov/stations/';
+const latest_obs2 = '/observations/latest';
+
 app.get("/", (req, res) => {
   res.json({ message: "Server Working" });
-  axios.get('https://api.weather.gov/stations/SURF1/observations/latest').then(
-    function(Response){
-      onSuccess(Response)
-    }).catch(function(error){
-      console.log(error)
+
+  for (const x in states){
+    axios.get(`${baseIPA}`+states[x]).then(
+      (Response)=>{
+        console.log(`${baseIPA}`+states[x]);
+        for (const y in Response.data.features){
+          const stations_id = Response.data.features[y].id;
+          const stations = stations_id.substring(33,40);
+          const latest = stations;
+          const last_obs_stat = latest_obs1+latest+latest_obs2;
+          
+          axios.get((`${last_obs_stat}`)).then(
+            (Response)=>{
+              if (Response.status === 200){
+
+                axios.get(last_obs_stat).then(
+                  function(Response){ onSuccess(Response) }).catch(function(error){ console.log(error) });
+                
+                  function onSuccess(Response){
+                    
+                    var props = Response.data.properties;
+                    var coords = Response.data.geometry;
+                
+                    var geometry = coords;
+                    var timeStamp = props.timestamp;
+                    var temperature = props.temperature;
+                    var windDirection = props.windDirection;
+                    var windSpeed = props.windSpeed;
+                    var Pressure = props.barometricPressure;
+                    var visibility = props.visibility;
+                    var maxTemp24h = props.maxTemperatureLast24Hours;
+                    var minTemp24h = props.minTemperatureLast24Hours;
+                    var Humidity = props.relativeHumidity;
+                    var station = props.station;
+                    var elevation = props.elevation;
+                
+                    assignDataValue(geometry,timeStamp,temperature,windSpeed,windDirection,
+                      Pressure,visibility,maxTemp24h,minTemp24h,Humidity,station,elevation)
+                  }
+                
+                  function assignDataValue(geometry,timeStamp,temperature,windSpeed,windDirection,
+                    Pressure,visibility,maxTemp24h,minTemp24h,Humidity,station,elevation){
+                
+                      var pushData = new Data();
+                      pushData.geometry = geometry;
+                      pushData.timeStamp = timeStamp;
+                      pushData.temperature = temperature;
+                      pushData.windSpeed = windSpeed;
+                      pushData.windDirection = windDirection;
+                      pushData.Pressure = Pressure;
+                      pushData.visibility = visibility;
+                      pushData.maxTemp24h = maxTemp24h;
+                      pushData.minTemp24h = minTemp24h;
+                      pushData.Humidity = Humidity;
+                      pushData.station = station;
+                      pushData.elevation = elevation;
+                
+                      pushData.save();
+                    }
+
+              }else{;}
+              setTimeout(() => { }, 3000)
+            }).catch( err => {
+              //console.clear(err);
+            });
+          }
+      }
+    ).catch( err => {
+      //console.error(err);
     });
+  } 
+
+  /*axios.get('https://api.weather.gov/stations/K79J/observations/latest').then(
+    function(Response){ onSuccess(Response) }).catch(function(error){ console.log(error) });
   
     function onSuccess(Response){
-    var json = Response.data.properties;
-    console.log(json);
-    var timeStamp = json.timestamp;
-    var temperature = json.temperature;
-    var windDirection = json.windDirection;
-    var windSpeed = json.windSpeed;
-    var Pressure = json.barometricPressure;
-    var visibility = json.visibility;
-    var maxTemp24h = json.maxTemperatureLast24Hours;
-    var minTemp24h = json.minTemperatureLast24Hours;
-    var Humidity = json.relativeHumidity;
-    var station = json.station;
-    var elevation = json.elevation;
-
-    assignDataValue(timeStamp,temperature,windSpeed,windDirection,
-      Pressure,visibility,maxTemp24h,minTemp24h,Humidity,station,elevation)
-  }
-
-  function assignDataValue(timeStamp,temperature,windSpeed,windDirection,
-    Pressure,visibility,maxTemp24h,minTemp24h,Humidity,station,elevation){
-      var pushData = new Data()
-      pushData.timeStamp = timeStamp;
-      pushData.temperature = temperature;
-      pushData.windSpeed = windSpeed;
-      pushData.windDirection = windDirection;
-      pushData.Pressure = Pressure;
-      pushData.visibility = visibility;
-      pushData.maxTemp24h = maxTemp24h;
-      pushData.minTemp24h = minTemp24h;
-      pushData.Humidity = Humidity;
-      pushData.station = station;
-      pushData.elevation = elevation;
-
-      pushData.save();
+      
+      var props = Response.data.properties;
+      var coords = Response.data.geometry;
+  
+      var geometry = coords;
+      var timeStamp = props.timestamp;
+      var temperature = props.temperature;
+      var windDirection = props.windDirection;
+      var windSpeed = props.windSpeed;
+      var Pressure = props.barometricPressure;
+      var visibility = props.visibility;
+      var maxTemp24h = props.maxTemperatureLast24Hours;
+      var minTemp24h = props.minTemperatureLast24Hours;
+      var Humidity = props.relativeHumidity;
+      var station = props.station;
+      var elevation = props.elevation;
+  
+      assignDataValue(geometry,timeStamp,temperature,windSpeed,windDirection,
+        Pressure,visibility,maxTemp24h,minTemp24h,Humidity,station,elevation)
     }
+  
+    function assignDataValue(geometry,timeStamp,temperature,windSpeed,windDirection,
+      Pressure,visibility,maxTemp24h,minTemp24h,Humidity,station,elevation){
+  
+        var pushData = new Data();
+        pushData.geometry = geometry;
+        pushData.timeStamp = timeStamp;
+        pushData.temperature = temperature;
+        pushData.windSpeed = windSpeed;
+        pushData.windDirection = windDirection;
+        pushData.Pressure = Pressure;
+        pushData.visibility = visibility;
+        pushData.maxTemp24h = maxTemp24h;
+        pushData.minTemp24h = minTemp24h;
+        pushData.Humidity = Humidity;
+        pushData.station = station;
+        pushData.elevation = elevation;
+  
+        pushData.save();
+      }*/
+ 
 });
 
 app.listen(PORT, (req, res) => {
--- react-app/server/model/data.js
+++ react-app/server/model/data.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
+var Schema   = mongoose.Schema;
 
-const DataSchema = mongoose.Schema({
+const objects = new Schema({
+    //geometry: {type: String, coordinates: [Number, Number] },
+    //geometry: {type: String, coordinates: [Number, Number]},
     elevation: {unitCode: String, value: Number},
     station: String,
     timeStamp: String,
@@ -12,5 +15,10 @@ const DataSchema = mongoose.Schema({
     maxTemp24h: {unitCode: String, value: Number},
     minTemp24h: {unitCode: String, value: Number},
     Humidity: {unitCode: String, value: Number}
-})
-module.exports = mongoose.model("data", DataSchema); 
+},
+{ typeKey: '$type' } //needed for moongose to not recon type as a special word but a normal string insted
+)
+
+//objects.index({geometry:'2dsphere'});
+
+module.exports = mongoose.model("data", objects); 
